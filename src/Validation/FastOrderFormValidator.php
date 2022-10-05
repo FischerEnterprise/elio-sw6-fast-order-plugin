f3704a7de2485707d5b852534d5f9d95f65255f2
Ben Fischer <ben@fischer-enterprise.de>
2022-10-05 16:23:25 +0200

    Add product quantity validation

--- src/Validation/FastOrderFormValidator.php
+++ src/Validation/FastOrderFormValidator.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Febf\FastOrderPlugin\Validation;
 
 use Febf\FastOrderPlugin\Validation\Constraints\ValidProductNumber;
+use Febf\FastOrderPlugin\Validation\Constraints\ValidProductQuantity;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\Validation\BuildValidationEvent;
 use Shopware\Core\Framework\Validation\DataBag\DataBag;
+use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
 use Shopware\Core\Framework\Validation\DataValidationDefinition;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 
@@ -26,14 +28,19 @@ class FastOrderFormValidator
 
     private const VALIDATION_NAME = 'fast_checkout_form.validate';
 
-    public function validate(SalesChannelContext $salesChannelContext): DataValidationDefinition
+    public function validate(SalesChannelContext $salesChannelContext, RequestDataBag $data): DataValidationDefinition
     {
         $definition = new DataValidationDefinition(self::VALIDATION_NAME);
 
         for ($i = 0; $i < 11; $i++) {
             $definition->add('fast-order-article-' . $i, new ValidProductNumber([
                 'salesChannelContext' => $salesChannelContext,
-                'productRepository' => $this->productRepository
+                'productRepository' => $this->productRepository,
+            ]));
+            $definition->add('fast-order-qtty-' . $i, new ValidProductQuantity([
+                'salesChannelContext' => $salesChannelContext,
+                'productRepository' => $this->productRepository,
+                'productNumber' => $data->get('fast-order-article-' . $i),
             ]));
         }
 
